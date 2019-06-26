1254a9635e3f16cc19a2de482dfc94d55e9205f1
Altamatick <altamatick@gmail.com>
2019-06-26 12:40:10 +0300

    composer

---
+++ user/Developer.php
@@ -0,0 +1,18 @@
+<?php
+namespace altamatick\test\user;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Developer extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'User Creator',
+            '============',
+            '',
+        ]);
+    }
+}
